40375afcf7018ad2dcab03976631d429b9d47276
oisin94 <100865322+oisin94@users.noreply.github.com>
2024-01-12 12:21:29 +0000

    Initial commit

---
+++ src/main/java/com/mmamanagement/MmaManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mmamanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmaManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmaManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmamanagement/MmaManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmamanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmaManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
